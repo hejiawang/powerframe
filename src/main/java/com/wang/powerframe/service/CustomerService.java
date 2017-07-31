60045fba22bb64fe539fa316b64200ecf42c263c
hejiawang <952327407@qq.com>
2017-07-31 22:03:52 +0800

    servlet modlel

--- src/main/java/com/wang/powerframe/service/CustomerService.java
+++ src/main/java/com/wang/powerframe/service/CustomerService.java
@@ -1,9 +1,21 @@
 package com.wang.powerframe.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.wang.powerframe.model.Customer;
+import com.wang.powerframe.util.CastUtil;
+import com.wang.powerframe.util.PropsUtil;
 
 /**
  * 提供客户数据服务
@@ -12,13 +24,62 @@ import com.wang.powerframe.model.Customer;
  */
 public class CustomerService {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+	
+	private static final String DRIVER;
+	private static final String URL;
+	private static final String USERNAME;
+	private static final String PASSWORD;
+	
+	static {
+		Properties conf = PropsUtil.loadProps("config.properties");
+		DRIVER = conf.getProperty("jdbc.driver");
+		URL = conf.getProperty("jdbc.url");
+		USERNAME = conf.getProperty("jdbc.username");
+		PASSWORD = conf.getProperty("jdbc.password");
+		
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			LOGGER.error("can not load jdbc driver", e);
+		}
+	}
+	
 	/**
 	 * 获取客户列表
 	 * @return
 	 */
 	public List<Customer> getCustomerList(){
-		// TODO
-		return null;
+		Connection conn = null;
+		List<Customer> customerList = new ArrayList<Customer>();
+		try {
+			
+			String sql = "select * from customer";
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			while( rs.next() ) {
+				Customer customer = new Customer();
+				customer.setId(rs.getLong("id"));
+				customer.setName(rs.getString("name"));
+				customer.setContact(rs.getString("contact"));
+				customer.setTelephone(rs.getString("telephone"));
+				customer.setEmail(rs.getString("email"));
+				customer.setRemark(rs.getString("remark"));
+				customerList.add(customer);
+			}
+		} catch (SQLException e) {
+			LOGGER.error("execute sql failure", e);
+		} finally {
+			if( conn != null ) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					LOGGER.error("close connection failure", e);
+				}
+			}
+		}
+		return customerList;
 	}
 	
 	/**
@@ -27,8 +88,34 @@ public class CustomerService {
 	 * @return 客户
 	 */
 	public Customer getCustomer(long id) {
-		// TODO
-		return null;
+		Connection conn = null;
+		Customer customer = new Customer();
+		try {
+			
+			String sql = "select * from customer where id = " + id;
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			while( rs.next() ) {
+				customer.setId(rs.getLong("id"));
+				customer.setName(rs.getString("name"));
+				customer.setContact(rs.getString("contact"));
+				customer.setTelephone(rs.getString("telephone"));
+				customer.setEmail(rs.getString("email"));
+				customer.setRemark(rs.getString("remark"));
+			}
+		} catch (SQLException e) {
+			LOGGER.error("execute sql failure", e);
+		} finally {
+			if( conn != null ) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					LOGGER.error("close connection failure", e);
+				}
+			}
+		}
+		return customer;
 	}
 	
 	/**
@@ -37,8 +124,32 @@ public class CustomerService {
 	 * @return
 	 */
 	public boolean createCustomer(Map<String, Object> fieldMap) {
-		// TODO
-		return false;
+		Connection conn = null;
+		int result = 0;
+		try {
+			
+			String sql = "insert into customer(name,contact,telephone,email,remark) values (?, ?, ?, ?, ?)";
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, CastUtil.castString(fieldMap.get("name")));
+			stmt.setString(2, CastUtil.castString(fieldMap.get("contact")));
+			stmt.setString(3, CastUtil.castString(fieldMap.get("telephone")));
+			stmt.setString(4, CastUtil.castString(fieldMap.get("email")));
+			stmt.setString(5, CastUtil.castString(fieldMap.get("remark")));
+			result = stmt.executeUpdate();
+		} catch (SQLException e) {
+			LOGGER.error("execute sql failure", e);
+		} finally {
+			if( conn != null ) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					LOGGER.error("close connection failure", e);
+				}
+			}
+		}
+		
+		return result >= 1 ;
 	}
 	
 	/**
@@ -48,8 +159,33 @@ public class CustomerService {
 	 * @return
 	 */
 	public boolean updateCustomer( long id, Map<String, Object> fieldMap ) {
-		// TODO
-		return false;
+		Connection conn = null;
+		int result = 0;
+		try {
+			
+			String sql = "update customer set name =?, contact = ?, telephone = ?, email = ?, remark = ? where id = ?";
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, CastUtil.castString(fieldMap.get("name")));
+			stmt.setString(2, CastUtil.castString(fieldMap.get("contact")));
+			stmt.setString(3, CastUtil.castString(fieldMap.get("telephone")));
+			stmt.setString(4, CastUtil.castString(fieldMap.get("email")));
+			stmt.setString(5, CastUtil.castString(fieldMap.get("remark")));
+			stmt.setString(6, CastUtil.castString(id));
+			result = stmt.executeUpdate();
+		} catch (SQLException e) {
+			LOGGER.error("execute sql failure", e);
+		} finally {
+			if( conn != null ) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					LOGGER.error("close connection failure", e);
+				}
+			}
+		}
+		
+		return result >= 1 ;
 	}
 	
 	/**
@@ -58,7 +194,27 @@ public class CustomerService {
 	 * @return
 	 */
 	public boolean deleteCustomer( long id ) {
-		// TODO
-		return false;
+		Connection conn = null;
+		int result = 0;
+		try {
+			
+			String sql = "delete from customer where id = ?";
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, CastUtil.castString(id));
+			result = stmt.executeUpdate();
+		} catch (SQLException e) {
+			LOGGER.error("execute sql failure", e);
+		} finally {
+			if( conn != null ) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					LOGGER.error("close connection failure", e);
+				}
+			}
+		}
+		
+		return result >= 1 ;
 	}
 }
--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,12 +1,13 @@
-create table customer {
-	id bigint(20) not null auto_increment,
-	name varchar(255) default null,
-	contact varchar(255) default null,
-	telephone varchar(255) default null,
-	email varchar(255) default null,
-	remark text,
-	primary key ('id')	
-} ENGINE = InnoDB DEFAULT CHARSET = utf8
+
+CREATE TABLE `customer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(255) DEFAULT NULL COMMENT '名称',
+  `contact` varchar(255) DEFAULT NULL COMMENT '联系人',
+  `telephone` varchar(255) DEFAULT NULL COMMENT '联系电话',
+  `email` varchar(255) DEFAULT NULL COMMENT '有限',
+  `remark` text COMMENT '备注',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into customer values ('1', 'customer1', 'Jack', '13889259343', '952327407@qq.com', null);
 insert into customer values ('2', 'customer2', 'Rose', '13889259344', '952327408@qq.com', null);
