0ab0afbaf4b00f3526861076da36798badb75bec
hejiawang <952327407@qq.com>
2017-08-01 21:27:20 +0800

    dbutil

--- src/main/java/com/wang/powerframe/helper/DatabaseHelper.java
+++ src/main/java/com/wang/powerframe/helper/DatabaseHelper.java
@@ -3,8 +3,14 @@ package com.wang.powerframe.helper;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,8 +22,21 @@ import com.wang.powerframe.util.PropsUtil;
  * @date   2017.07.31
  */
 public final class DatabaseHelper {
-	
+
+	/**
+	 * logger manager
+	 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
+	
+	/**
+	 * dbutil
+	 */
+	private static final QueryRunner QUERY_RUNNER;
+	
+	/**
+	 * 隔离线程的容器
+	 */
+	private static final ThreadLocal<Connection> CONNECTION_HOLDER ;
 	
 	private static final String DRIVER;
 	private static final String URL;
@@ -25,6 +44,10 @@ public final class DatabaseHelper {
 	private static final String PASSWORD;
 	
 	static {
+		QUERY_RUNNER = new QueryRunner();
+		
+		CONNECTION_HOLDER = new ThreadLocal<Connection>();
+		
 		Properties conf = PropsUtil.loadProps("config.properties");
 		DRIVER = conf.getProperty("jdbc.driver");
 		URL = conf.getProperty("jdbc.url");
@@ -38,16 +61,23 @@ public final class DatabaseHelper {
 		}
 	}
 	
+	
 	/**
 	 * 获取数据库链接
 	 * @return
 	 */
 	public static Connection getConnection() {
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-		} catch (SQLException e) {
-			LOGGER.error("get connection failure", e);
+		Connection conn = CONNECTION_HOLDER.get();
+		if( conn == null ) {
+			
+			try {
+				conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			} catch (SQLException e) {
+				LOGGER.error("get connection failure", e);
+				throw new RuntimeException(e);
+			} finally {
+				CONNECTION_HOLDER.set(conn);
+			}
 		}
 		
 		return conn;
@@ -57,13 +87,104 @@ public final class DatabaseHelper {
 	 * 关闭数据库链接
 	 * @param conn
 	 */
-	public  static void closeConnection( Connection conn ) {
+	public  static void closeConnection( ) {
+		Connection conn = CONNECTION_HOLDER.get();
 		if( conn != null ) {
+			
 			try {
 				conn.close();
 			} catch (SQLException e) {
 				LOGGER.error("close connection failure", e);
+				throw new RuntimeException(e);
+			} finally {
+				CONNECTION_HOLDER.remove();
 			}
 		}	
+	}
+	
+	/**
+	 * 查询实体列表
+	 * @param entityClass
+	 * @param sql
+	 * @param params
+	 * @return
+	 */
+	public static <T> List<T> queryEntityList( Class<T> entityClass, String sql, Object... params ) {
+		List<T> entityList;
+		try {
+			Connection conn = getConnection();
+			entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<T>(entityClass), params); 
+		} catch (SQLException e) {
+			LOGGER.error("query entity list failure", e);
+			throw new RuntimeException(e);
+		} finally {
+			closeConnection();
+		}
+		
+		return entityList;
+	}
+	
+	/**
+	 * 查询实体
+	 * @param entityClass
+	 * @param sql
+	 * @param params
+	 * @return
+	 */
+	public static <T> T queryEntity( Class<T> entityClass, String sql, Object...params ) {
+		T entity;
+		try {
+			Connection conn = getConnection();
+			entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T>(entityClass), params); 
+		} catch (SQLException e) {
+			LOGGER.error("query entity list failure", e);
+			throw new RuntimeException(e);
+		} finally {
+			closeConnection();
+		}
+		
+		return entity;
+	}
+	
+	/**
+	 * 执行查询语句
+	 * @param sql
+	 * @param params
+	 * @return
+	 */
+	public static List<Map<String, Object>> executeQuery( String sql, Object...params ) {
+		List<Map<String, Object>> entity;
+		try {
+			Connection conn = getConnection();
+			entity = QUERY_RUNNER.query(conn, sql, new MapListHandler(), params); 
+		} catch (SQLException e) {
+			LOGGER.error("query entity list failure", e);
+			throw new RuntimeException(e);
+		} finally {
+			closeConnection();
+		}
+		
+		return entity;
+	}
+	
+	/**
+	 * 执行更新语句 ( 包括 update, insert, delete ) 
+	 * @param sql
+	 * @param params
+	 * @return 受影响的行数
+	 */
+	public static int executeUpdate( String sql, Object...params ) {
+		int rows = 0;
+		try {
+			Connection conn = getConnection();
+			rows = QUERY_RUNNER.update(conn, sql, params); 
+		} catch (SQLException e) {
+			LOGGER.error("query entity list failure", e);
+			throw new RuntimeException(e);
+		} finally {
+			closeConnection();
+		}
+		
+		return rows;
 	}
 }
--- src/main/java/com/wang/powerframe/service/CustomerService.java
+++ src/main/java/com/wang/powerframe/service/CustomerService.java
@@ -1,10 +1,5 @@
 package com.wang.powerframe.service;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,13 +8,13 @@ import org.slf4j.LoggerFactory;
 
 import com.wang.powerframe.helper.DatabaseHelper;
 import com.wang.powerframe.model.Customer;
-import com.wang.powerframe.util.CastUtil;
 
 /**
  * 提供客户数据服务
  * @author HeJiawang
  * @date   2017.07.30
  */
+@SuppressWarnings("unused")
 public class CustomerService {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
@@ -29,30 +24,8 @@ public class CustomerService {
 	 * @return
 	 */
 	public List<Customer> getCustomerList(){
-		Connection conn = null;
-		List<Customer> customerList = new ArrayList<Customer>();
-		try {
-			
-			String sql = "select * from customer";
-			conn = DatabaseHelper.getConnection();
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			ResultSet rs = stmt.executeQuery();
-			while( rs.next() ) {
-				Customer customer = new Customer();
-				customer.setId(rs.getLong("id"));
-				customer.setName(rs.getString("name"));
-				customer.setContact(rs.getString("contact"));
-				customer.setTelephone(rs.getString("telephone"));
-				customer.setEmail(rs.getString("email"));
-				customer.setRemark(rs.getString("remark"));
-				customerList.add(customer);
-			}
-		} catch (SQLException e) {
-			LOGGER.error("execute sql failure", e);
-		} finally {
-			DatabaseHelper.closeConnection(conn);
-		}
-		return customerList;
+		String sql = "select * from customer";
+		return DatabaseHelper.queryEntityList(Customer.class, sql);
 	}
 	
 	/**
@@ -61,28 +34,8 @@ public class CustomerService {
 	 * @return 客户
 	 */
 	public Customer getCustomer(long id) {
-		Connection conn = null;
-		Customer customer = new Customer();
-		try {
-			
-			String sql = "select * from customer where id = " + id;
-			conn = DatabaseHelper.getConnection();
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			ResultSet rs = stmt.executeQuery();
-			while( rs.next() ) {
-				customer.setId(rs.getLong("id"));
-				customer.setName(rs.getString("name"));
-				customer.setContact(rs.getString("contact"));
-				customer.setTelephone(rs.getString("telephone"));
-				customer.setEmail(rs.getString("email"));
-				customer.setRemark(rs.getString("remark"));
-			}
-		} catch (SQLException e) {
-			LOGGER.error("execute sql failure", e);
-		} finally {
-			DatabaseHelper.closeConnection(conn);
-		}
-		return customer;
+		String sql = "select * from customer where id = " + id;
+		return DatabaseHelper.queryEntity(Customer.class, sql);
 	}
 	
 	/**
@@ -91,26 +44,8 @@ public class CustomerService {
 	 * @return
 	 */
 	public boolean createCustomer(Map<String, Object> fieldMap) {
-		Connection conn = null;
-		int result = 0;
-		try {
-			
-			String sql = "insert into customer(name,contact,telephone,email,remark) values (?, ?, ?, ?, ?)";
-			conn = DatabaseHelper.getConnection();
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setString(1, CastUtil.castString(fieldMap.get("name")));
-			stmt.setString(2, CastUtil.castString(fieldMap.get("contact")));
-			stmt.setString(3, CastUtil.castString(fieldMap.get("telephone")));
-			stmt.setString(4, CastUtil.castString(fieldMap.get("email")));
-			stmt.setString(5, CastUtil.castString(fieldMap.get("remark")));
-			result = stmt.executeUpdate();
-		} catch (SQLException e) {
-			LOGGER.error("execute sql failure", e);
-		} finally {
-			DatabaseHelper.closeConnection(conn);
-		}
-		
-		return result >= 1 ;
+		String sql = "insert into customer(name,contact,telephone,email,remark) values (?, ?, ?, ?, ?)";
+		return DatabaseHelper.executeUpdate(sql) > 0;
 	}
 	
 	/**
@@ -120,27 +55,8 @@ public class CustomerService {
 	 * @return
 	 */
 	public boolean updateCustomer( long id, Map<String, Object> fieldMap ) {
-		Connection conn = null;
-		int result = 0;
-		try {
-			
-			String sql = "update customer set name =?, contact = ?, telephone = ?, email = ?, remark = ? where id = ?";
-			conn = DatabaseHelper.getConnection();
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setString(1, CastUtil.castString(fieldMap.get("name")));
-			stmt.setString(2, CastUtil.castString(fieldMap.get("contact")));
-			stmt.setString(3, CastUtil.castString(fieldMap.get("telephone")));
-			stmt.setString(4, CastUtil.castString(fieldMap.get("email")));
-			stmt.setString(5, CastUtil.castString(fieldMap.get("remark")));
-			stmt.setString(6, CastUtil.castString(id));
-			result = stmt.executeUpdate();
-		} catch (SQLException e) {
-			LOGGER.error("execute sql failure", e);
-		} finally {
-			DatabaseHelper.closeConnection(conn);
-		}
-		
-		return result >= 1 ;
+		String sql = "update customer set name =?, contact = ?, telephone = ?, email = ?, remark = ? where id = ?";
+		return DatabaseHelper.executeUpdate(sql, fieldMap.get("name"), fieldMap.get("contact"), fieldMap.get("telephone"), fieldMap.get("email"), fieldMap.get("remark")) > 0;
 	}
 	
 	/**
@@ -149,21 +65,7 @@ public class CustomerService {
 	 * @return
 	 */
 	public boolean deleteCustomer( long id ) {
-		Connection conn = null;
-		int result = 0;
-		try {
-			
-			String sql = "delete from customer where id = ?";
-			conn = DatabaseHelper.getConnection();
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setString(1, CastUtil.castString(id));
-			result = stmt.executeUpdate();
-		} catch (SQLException e) {
-			LOGGER.error("execute sql failure", e);
-		} finally {
-			DatabaseHelper.closeConnection(conn);
-		}
-		
-		return result >= 1 ;
+		String sql = "delete from customer where id = ?";
+		return DatabaseHelper.executeUpdate(sql, id) > 0;
 	}
 }
